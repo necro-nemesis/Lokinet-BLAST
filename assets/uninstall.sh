0d60b45d39a15b4463b756c56e186bdbfcd3282e
glen <glen@debian.home>
2021-03-05 17:25:05 -0500

    edit

--- assets/uninstall.sh
+++ assets/uninstall.sh
@@ -4,4 +4,5 @@ sudo apt remove lokinet-gui -y && sudo apt remove lokinet -y
 sudo systemctl restart dhcpcd
 sudo rm /usr/share/applications/Lokinet.desktop || install_error "Unable to remove startup entry"
 sudo rm /usr/pixmaps/lokiremove.png || install_error "Unable to remove startup icon"
-sudo rm -r /etc/lokinet || install_error "Unable to remove lokinet removal files"
+sudo rm -r /tmp/lokinet || install_error "Unable to remove lokinet removal files"
+sudo rm -r /tmp/lokinet.* || install_error "Unable to remove lokinet removal files"
