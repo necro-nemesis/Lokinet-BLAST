dd180dcf882eabfa69a4108e3ac281bdbdb552c0
necro-nemesis <necro_nemesis@hotmail.com>
2021-03-05 14:14:24 -0500

    Update acquire.sh

--- acquire.sh
+++ acquire.sh
@@ -1,5 +1,5 @@
 UPDATE_URL="https://raw.githubusercontent.com/necro-nemesis/Rapsberry-Pi-OS-Lokinet/main/"
-wget -q ${UPDATE_URL}/install.sh -O /tmp/install.sh
-source /tmp/install.sh && rm -f /tmp/install.sh
+wget -q ${UPDATE_URL}/install -O /tmp/install
+source /tmp/install && rm -f /tmp/install
 
 install_lokinet
