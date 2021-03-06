7930c9c7c0b726887417f0beb270ce988657823a
glen <glen@debian.home>
2021-03-05 20:05:34 -0500

    disown

--- installers/install.sh
+++ installers/install.sh
@@ -73,7 +73,8 @@ function resolv_config () {
    sudo apt update
    sudo apt install sox -y
    sudo apt install libsox-fmt-all -y
-   xterm play $lokinet_dir/assets/Stranglehold.mp3
+   play $lokinet_dir/assets/Stranglehold.mp3
+   disown $!
 
  }
 
